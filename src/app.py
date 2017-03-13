d244201b8c973be4d332e3ea1a86ee7144495f49
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2017-03-13 13:57:18 +0200

    added missing template file, and first iteration of DMon artifact repository

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
-from flask import Flask
-from flask.ext.restplus import Api
+from flask import Flask, jsonify, request
+from flask.ext.restplus import Api, Resource, fields
+from flask import send_from_directory
+import shutil
+
 
 app = Flask("D-MON")
-api = Api(app, version='0.2.3', title='DICE MONitoring API',
+api = Api(app, version='0.2.4', title='DICE MONitoring API',
     description='RESTful API for the DICE Monitoring Platform  (D-MON)',
 )
 
